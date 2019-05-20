af90e97a8605f36cf7e59b1e1e6758707b671ee5
shigeki chiba <shigegle@gmail.com>
2019-05-20 17:12:20 +0000

    メモ化による高速化

--- app.js
+++ app.js
@@ -3,21 +3,25 @@
 
 'use strict';
 
-function fibonacci(n)
+const memo = new Map();
+
+memo.set(0, 0);
+memo.set(1, 1);
+
+function fib(n)
 {
-    if(n === 0)
+    if(memo.has(n))
     {
-        return 0;
+        return memo.get(n);
     }
-    else if(n === 1)
-    {
-        return 1;
-    }
-    return fibonacci(n-1) + fibonacci(n-2);
+
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n, value);
+    return value;
 }
 
 const length = 40;
 for (let i = 0 ; i <= length ; ++i)
 {
-    console.log(fibonacci(i));
+    console.log(fib(i));
 }
